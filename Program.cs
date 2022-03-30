c8fe476e6b2114f8ab618f57f438a3bad66fdc6e
Daniele Romes <71996747+Danny-ctrl@users.noreply.github.com>
2022-03-30 15:42:30 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Globalization;
+
+namespace Moedas
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.Clear();
+      decimal valor = 10567.23m;
+
+      System.Console.WriteLine(
+        Math.Round(valor)
+      );
+      System.Console.WriteLine(
+        Math.Ceiling(valor)
+      );
+      System.Console.WriteLine(
+        Math.Floor(valor)
+      );
+
+
+      System.Console.WriteLine(
+         valor.ToString(
+       "C",
+        CultureInfo.CreateSpecificCulture("pt-BR")
+       )
+         );
+    }
+  }
+}
